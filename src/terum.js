c13d8076187ba862100f66b12a0bf0df82c82903
Ajay Chawda <manya@Ajays-MacBook-Pro.local>
2022-09-08 15:07:06 +0530

    fixes

--- src/terum.js
+++ src/terum.js
@@ -8,7 +8,7 @@ const terumObject = {
     'audience': 'everyone',
     'goal': 'share_knowledge',
     'description': 'San Francisco’s Coit Tower hike is a beautiful one with stairs through secret gardens of the city!',
-    'user_id': 1 ,  
+    'user_id': 6 ,  
     'status': 'ACTIVE',
     'expired_at': '2022-08-27 22:48:55.149519',
     'start_latitude': '23.3635810000',
@@ -21,20 +21,28 @@ const terumObject = {
     'five_day_notification_sent': '0',
     'activist_goal': 'activist_50',
     'is_goal_reached': '0',
+    
     'popularity': '10',
-    'inviteToFollowers': '0'
+    'inviteToFollowers': '0',
+    'start_date': '2022-09-01 07:00:00.000000',
+    'end_date': '2023-10-06 07:00:00.000000',
 }
 
-const insert = async (connection, index) => { 
+const insert = async (connection, index, type) => { 
     let explanation = 'LOAD_'+index+'_'+terumObject.explanation;
-    let sqlQuery = "INSERT INTO `terum`.`social_terum` (`created_at`, `updated_at`, `explanation`, `audience`, `goal`, `description`, `user_id`, `status`, `expired_at`, `start_latitude`, `start_longitude`, `target_latitude`, `target_longitude`, `created`, `points`, `shares`, `five_day_notification_sent`, `activist_goal`, `is_goal_reached`, `popularity`, `inviteToFollowers`) VALUES ('"+terumObject.created_at+"','"+terumObject.updated_at+"', '"+explanation+"', '"+terumObject.audience+"', '"+terumObject.goal+"', '"+terumObject.description+"', "+terumObject.user_id+", '"+terumObject.status+"', '"+terumObject.expired_at+"',"+terumObject.start_latitude+", "+terumObject.start_longitude+", "+terumObject.target_latitude+", "+terumObject.target_longitude+", '"+terumObject.created+"', '"+terumObject.points+"', '"+terumObject.shares+"', '"+terumObject.five_day_notification_sent+"', '"+terumObject.activist_goal+"', '"+terumObject.is_goal_reached+"', '"+terumObject.popularity+"', '"+terumObject.inviteToFollowers+"')";
+    let sqlQuery = "INSERT INTO `terum`.`social_terum` (`created_at`, `updated_at`, `explanation`, `audience`, `goal`, `description`, `user_id`, `status`, `expired_at`, `start_latitude`, `start_longitude`, `target_latitude`, `target_longitude`, `created`, `points`, `shares`, `five_day_notification_sent`, `activist_goal`, `is_goal_reached`, `popularity`, `inviteToFollowers`,`start_date`, `end_date`) VALUES ('"+terumObject.created_at+"','"+terumObject.updated_at+"', '"+explanation+"', '"+terumObject.audience+"', '"+type+"', '"+terumObject.description+"', "+terumObject.user_id+", '"+terumObject.status+"', '"+terumObject.expired_at+"',"+terumObject.start_latitude+", "+terumObject.start_longitude+", "+terumObject.target_latitude+", "+terumObject.target_longitude+", '"+terumObject.created+"', '"+terumObject.points+"', '"+terumObject.shares+"', '"+terumObject.five_day_notification_sent+"', '"+terumObject.activist_goal+"', '"+terumObject.is_goal_reached+"', '"+terumObject.popularity+"', '"+terumObject.inviteToFollowers+"', '"+terumObject.start_date+"', '"+terumObject.end_date+"')";
     try {
         console.log('before insert', index);
         let results = await connection.query(sqlQuery);
         if(results[0]?.insertId){
             
             //Add new media ie upload a single image
-            sqlQuery = "INSERT INTO social_media (`created_at`, `updated_at`, `file`, `media_type`, `description`, `user_id`, `thumbnail`, `thumbnail_created`) VALUES ('2022-07-29 09:46:09.647584', '2022-07-29 09:46:09.647628', '03998795-717B-4EF3-932E-8C21737EF99A.png', 'image', 'challenge-images', '1', '', '0')"
+            let image = '1659349234973.jpg';
+            if(type == 'challenge_others') {
+                image = '1659355682951.PNG';
+            }
+
+            sqlQuery = "INSERT INTO social_media (`created_at`, `updated_at`, `file`, `media_type`, `description`, `user_id`, `thumbnail`, `thumbnail_created`) VALUES ('2022-07-29 09:46:09.647584', '2022-07-29 09:46:09.647628', '"+image+"', 'image', 'challenge-images', '1', '', '0')"
             let uploadResult = await connection.query(sqlQuery);
             
             //Link that image to terum
@@ -59,7 +67,10 @@ const insert = async (connection, index) => {
 const createTerum = async (count) => {
     const connection = await db.getConnection();
     for(let index=0; index<count; index++) {
-        await insert(connection, index);
+        await insert(connection, index, 'share_knowledge');
+    }
+    for(let index=0; index<count; index++) {
+        await insert(connection, index, 'challenge_others');
     }
 }
 
